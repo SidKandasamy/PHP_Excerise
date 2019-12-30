cae1ebdfaa5ac2ee4c99a0b4445dff42e9b17e77
Sid Kandasamy <sidkandasamy@outlook.com>
2019-12-30 13:50:26 +0000

    vagrant issues, unable to see in browser

---
+++ public/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>PHP Caluculator Excerise</title>
+</head>
+<body>
+    
+<form action="/action_page.php">
+  <input type="text" name="num1" placeholder="Number 1">
+  <input type="text" name="num2" placeholder="Number 2">
+  <input type="submit" value="Submit"> 
+</form> 
+
+    
+</body>
+</html>

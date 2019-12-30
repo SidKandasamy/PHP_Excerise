a550228ba6b8fe21226a3277487da9b3345111cd
Sid Kandasamy <sidkandasamy@outlook.com>
2019-12-30 13:11:48 +0000

    form created

--- index.php
+++ index.php
@@ -8,6 +8,15 @@
 </head>
 <body>
     
+<form action="/action_page.php">
+  First name:<br>
+  <input type="text" name="firstname" value="">
+  <br>
+  Last name:<br>
+  <input type="text" name="lastname" value="">
+  <br><br>
+  <input type="submit" value="Submit">
+</form> 
 
     
 </body>
